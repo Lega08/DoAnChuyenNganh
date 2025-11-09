0fec8cdd7f1efaa6aaef80a2f658e66734965a2f
Vu <nvu60803@gmail.com>
2025-11-09 18:57:10 +0700

    Bao cao tuan 4

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -3,15 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const {engine} = require('express-handlebars');
+var app = express();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var shopRouter = require('./routes/shop');
-var singleRouter = require('./routes/single');
+var adminRouter = require('./routes/admin');
 
-var app = express();
 
-// view engine setup
+app.engine('hbs', engine({
+    extname: '.hbs',
+    defaultLayout: 'layouts',
+    partialsDir: path.join(__dirname, 'views', 'partials'),
+    layoutsDir: path.join(__dirname, 'views', 'layouts')
+}));
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
@@ -23,23 +29,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/shop', shopRouter);
-app.use('/single', singleRouter);
+app.use('/admin', adminRouter);
 
-// catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
